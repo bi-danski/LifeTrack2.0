f8c10cc5da574b94ab4242ddfd8950fcb51ef968
bi-danski <bidanski666@gmail.com>
2025-11-14 10:06:07 +0300

    rewiring

---
+++ app/src/main/java/org/lifetrack/ltapp/model/repository/AuthRepositoryImpl.kt
@@ -0,0 +1,33 @@
+package org.lifetrack.ltapp.model.repository
+
+import org.lifetrack.ltapp.model.data.dto.AuthResult
+
+class AuthRepositoryImpl: AuthRepository {
+    override suspend fun login(
+        email: String,
+        password: String
+    ): AuthResult {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun signUp(
+        email: String,
+        password: String,
+        phoneNumber: String,
+        displayName: String
+    ): AuthResult {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun refreshSession(): AuthResult {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun getTokenId(): String {
+        TODO("Not yet implemented")
+    }
+
+    override fun logout(): AuthResult {
+        TODO("Not yet implemented")
+    }
+}
