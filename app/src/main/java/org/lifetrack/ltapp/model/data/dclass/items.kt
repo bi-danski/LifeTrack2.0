4cf94b5023be78c6b10d35a9dc16c6a700dbf778
bi-danski <bidanski666@gmail.com>
2026-01-27 02:43:53 +0300

    (02) -> 43

--- app/src/main/java/org/lifetrack/ltapp/model/data/dclass/items.kt
+++ app/src/main/java/org/lifetrack/ltapp/model/data/dclass/items.kt
@@ -1,13 +1,8 @@
 package org.lifetrack.ltapp.model.data.dclass
 
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.CalendarMonth
-import androidx.compose.material.icons.filled.Cancel
-import androidx.compose.material.icons.filled.CheckCircle
 import androidx.compose.material.icons.filled.Emergency
-import androidx.compose.material.icons.filled.History
 import androidx.compose.material.icons.filled.Info
-import androidx.compose.material.icons.filled.NewReleases
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 
